efda3ec1c23f278781a77d29a462e25ddd15145d
sunweiliang <973891422@qq.com>
2022-08-21 00:07:03 +0800

    feat: add layout post pages

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
-// const isProd = process.env.NODE_ENV === 'production'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  // assetPrefix: isProd ? '/973891422/' : '',
-  images: {
-    loader: 'cloudinary',
-    path: '/',
-  },
 }
 module.exports = nextConfig
---
+++ test.ts
@@ -0,0 +1,4 @@
+const a: number = 'a'
+console.log(a)
+
+export {}
