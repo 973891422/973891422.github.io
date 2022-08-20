177baa0a55b1e9ecea8e13e55a8111641e39932b
sunweiliang <973891422@qq.com>
2022-08-20 18:24:38 +0800

    chore: add prettier husky lint-commit travis

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
+const isProd = process.env.NODE_ENV === 'production'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  assetPrefix: isProd ? '/973891422.github.io/' : ''
 }
-
 module.exports = nextConfig
