2db84a8aeb9d7e6f56cd215bfeccf83fd9d2803b
sunweiliang <973891422@qq.com>
2022-08-20 20:50:14 +0800

    chore: fix css mising

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
-const isProd = process.env.NODE_ENV === 'production'
+// const isProd = process.env.NODE_ENV === 'production'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  assetPrefix: isProd ? '/973891422/' : '',
+  // assetPrefix: isProd ? '/973891422/' : '',
   images: {
     loader: 'cloudinary',
     path: '/',
