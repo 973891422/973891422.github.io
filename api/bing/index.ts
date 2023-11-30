03650bd2bcf939eb2fee80b1eb11a3d5004044ec
sunweiliang <973891422@qq.com>
2023-11-30 17:28:35 +0800

    fix: app page error catch

--- api/bing/index.ts
+++ api/bing/index.ts
@@ -1,43 +1,72 @@
-export interface BingImages {
-  baseurl: string
-  toolTips: {
-    loading: string
-    previous: string
-    next: string
-    walle: string
-    walls: string
-  }
-  images: {
-    startdate: string
-    fullstartdate: string
-    enddate: string
-    url: string
-    urlbase: string
-    copyright: string
-    copyrightlink: string
-    title: string
-    quiz: string
-    wp: boolean
-    hsh: string
-    drk: number
-    top: number
-    bot: number
-    hs: []
-  }[]
-}
+// export interface BingImages {
+//   baseurl: string
+//   toolTips: {
+//     loading: string
+//     previous: string
+//     next: string
+//     walle: string
+//     walls: string
+//   }
+//   images: {
+//     startdate: string
+//     fullstartdate: string
+//     enddate: string
+//     url: string
+//     urlbase: string
+//     copyright: string
+//     copyrightlink: string
+//     title: string
+//     quiz: string
+//     wp: boolean
+//     hsh: string
+//     drk: number
+//     top: number
+//     bot: number
+//     hs: []
+//   }[]
+// }
+
+// export interface Image {
+//   startdate: string;
+//   fullstartdate: string;
+//   enddate: string;
+//   url: string;
+//   urlbase: string;
+//   copyright: string;
+//   copyrightlink: string;
+//   title: string;
+//   quiz: string;
+//   wp: boolean;
+//   hsh: string;
+//   drk: number;
+//   top: number;
+//   bot: number;
+//   hs: [];
+// }
+
+// export interface Tooltips {
+//   loading: string;
+//   previous: string;
+//   next: string;
+//   walle: string;
+//   walls: string;
+// }
+
+// export interface BingImages {
+//   images: Image[];
+//   tooltips: Tooltips;
+//   baseurl: string;
+// }
 // Effective：请求Bing背景图片API
 export const getBingBackgroundImage = async () => {
   // bing官方接口： idx=-1 截至明天， n=8 8张，ensearch=1 英文
   //   const enSearsh = (locale ?? defaultLocale) === 'en' ? '1' : '0'
-  const res = await fetch(
+  return await fetch(
     `https://cn.bing.com/HPImageArchive.aspx?format=js&idx=-1&n=8&mkt=zh-CN&ensearch=1`
   )
-  const result = await res.json()
-  if (!res.ok) {
-    throw new Error(
-      `Failed to fetch bing API pictures, received status ${res.status}`
-    )
-  }
-  result.baseurl = 'https://cn.bing.com'
-  return result as BingImages
+    .then((res) => res.json())
+    .then((res) => {
+      res.baseurl = 'https://cn.bing.com'
+      return res
+    })
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
-import { getBingBackgroundImage } from '../api/bing'
-import '../styles/globals.css'
+import { getBingBackgroundImage } from 'api/bing'
+import 'styles/globals.css'
 import { Metadata } from 'next'
 
 export const metadata: Metadata = {
@@ -7,26 +7,61 @@ export const metadata: Metadata = {
   description: 'Welcome to Next.js',
 }
 
-export default async function RootLayout({
+export default function RootLayout({
   // Layouts must accept a children prop.
   // This will be populated with nested layouts or pages
   children,
 }: {
   children: React.ReactNode
 }) {
-  const bing = await getBingBackgroundImage()
-  const imgURLs = bing.images.map((img) => {
-    return '' + bing.baseurl + img.url
-  })
-
-  return (
-    <html lang="en">
-      <body
-        className="h-screen w-screen bg-zinc-500 bg-cover bg-center bg-no-repeat"
-        style={{ backgroundImage: `url(${imgURLs[0]})` }}
-      >
-        {children}
-      </body>
-    </html>
-  )
+  return getBingBackgroundImage()
+    .then((bing) => {
+      return {
+        backgroundImage: `url(${'' + bing.baseurl + bing.images[0].url})`,
+      }
+    })
+    .catch(() => {
+      return {}
+    })
+    .then((styleObject) => {
+      return (
+        <html lang="en">
+          <body
+            className="h-screen w-screen bg-zinc-500 bg-cover bg-center bg-no-repeat"
+            style={styleObject}
+          >
+            {children}
+          </body>
+        </html>
+      )
+    })
 }
+
+// export default async function RootLayout({
+//   // Layouts must accept a children prop.
+//   // This will be populated with nested layouts or pages
+//   children,
+// }: {
+//   children: React.ReactNode
+// }) {
+//   let styleObject = {}
+//   try {
+//     const bing = await getBingBackgroundImage()
+//     styleObject = {
+//       backgroundImage: `url(${'' + bing.baseurl + bing.images[0].url})`,
+//     }
+//   } catch () {
+
+//   }
+
+//   return (
+//     <html lang="en">
+//       <body
+//         className="h-screen w-screen bg-zinc-500 bg-cover bg-center bg-no-repeat"
+//         style={styleObject}
+//       >
+//         {children}
+//       </body>
+//     </html>
+//   )
+// }
