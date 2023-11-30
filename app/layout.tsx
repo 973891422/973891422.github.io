6ba33bea0ae7ec75aa47269d90e0f9200dcbc02a
sunweiliang <973891422@qq.com>
2023-11-30 09:02:14 +0800

    fix: app layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { getBingBackgroundImage } from '../api/bing'
 import '../styles/globals.css'
 import { Metadata } from 'next'
 
@@ -6,16 +7,26 @@ export const metadata: Metadata = {
   description: 'Welcome to Next.js',
 }
 
-export default function RootLayout({
+export default async function RootLayout({
   // Layouts must accept a children prop.
   // This will be populated with nested layouts or pages
   children,
 }: {
   children: React.ReactNode
 }) {
+  const bing = await getBingBackgroundImage()
+  const imgURLs = bing.images.map((img) => {
+    return '' + bing.baseurl + img.url
+  })
+
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body
+        className="h-screen w-screen bg-zinc-500 bg-cover bg-center bg-no-repeat"
+        style={{ backgroundImage: `url(${imgURLs[0]})` }}
+      >
+        {children}
+      </body>
     </html>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,37 +1,3 @@
-// 副作用：请求Bing背景图片API
-interface BingImage {
-  url: string
-  title: string
-  copyright: string
-  copyrightlink: string
-}
-
-async function getBingBackgroundImage() {
-  // bing官方接口： idx=-1 截至明天， n=8 8张，ensearch=1 英文
-  //   const enSearsh = (locale ?? defaultLocale) === 'en' ? '1' : '0'
-  const res = await fetch(
-    `https://cn.bing.com/HPImageArchive.aspx?format=js&idx=-1&n=8&mkt=zh-CN&ensearch=1`
-  )
-  const result = await res.json()
-  if (!res.ok) {
-    throw new Error(
-      `Failed to fetch bing API pictures, received status ${res.status}`
-    )
-  }
-  const images = result.images.map((image: BingImage) => {
-    const url = 'https://cn.bing.com' + image.url
-    const title = image.title
-    const copyrightlink = image.copyrightlink
-    const regex = /([^]+) \(([^]+)\)/
-    const arr = regex.exec(image.copyright) ?? ['', '', '']
-    return { url, title, copyrightlink, copyright: [arr[1], arr[2]] }
-  })
-
-  return images
-}
-
 export default async function Page() {
-  const images = await getBingBackgroundImage()
-  console.log(images)
   return <main></main>
 }
